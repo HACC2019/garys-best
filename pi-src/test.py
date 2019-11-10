01c5779f3a602c89705b4e1856eae6811c3875ba
frendy <frendy@hawaii.edu>
2019-11-09 14:35:58 -1000

    FIxed small errors
    Ferrari license plate reads correctly.
    Error check if there is no contour.

--- pi-src/test.py
+++ pi-src/test.py
@@ -1,3 +1,10 @@
 import readlicense
 
 print(readlicense.readlicense('test_images/test2.jpg'))
+
+print(readlicense.readlicense('test_images/easyread.png'))
+
+print(readlicense.readlicense('test_images/test.jpg'))
+
+print(readlicense.readlicense('test_images/test3_(failed).jpg'))
+
