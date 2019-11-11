cc95d5fc1121670a6ff3035d38ebd9d21711cef9
matthewlamdotjs <matthewlam.js@gmail.com>
2019-11-11 13:34:45 -1000

    change to get req for serv change

--- pi-src/client.py
+++ pi-src/client.py
@@ -5,8 +5,8 @@ endpoint = 'https://hecoweb.azurewebsites.net/api/hardware/licenseplateatstation
   
 def to_svr(plate_no, station_name, date):
 
-    js_payload = '{LicensePlate: '+plate_no+', StationName: '+station_name+', Date: '+date+'}'
+    js_payload = '{"LicensePlate": "'+plate_no+'", "StationName": "'+station_name+'", "Date": "'+date+'"}'
 
     req_params = { 'json': js_payload }
     
-    r = requests.post(url = endpoint, data = req_params)
+    r = requests.get(url = endpoint, params = req_params)
