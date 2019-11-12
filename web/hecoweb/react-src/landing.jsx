067751071b6259631c8355ddcbfa73cd620599b5
Mercedez Castro <mercedez@hawaii.edu>
2019-11-12 13:01:53 -1000

    UI update

--- web/hecoweb/react-src/landing.jsx
+++ web/hecoweb/react-src/landing.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import 'semantic-ui-css/semantic.css';
 import ReactSpeedometer from "react-d3-speedometer";
-import { CircleMeter } from 'react-svg-meters'
+import { CircleMeter, DiskMeter } from 'react-svg-meters'
 import { Bar, Line } from 'react-chartjs-2';
-import { Grid, Menu, Container } from 'semantic-ui-react'
+import { Grid, Container, List, Segment, Message, Icon } from 'semantic-ui-react'
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
@@ -28,12 +29,23 @@ class Landing extends React.Component {
             }]
         };
 
-        const meterStyle = {
+        const useMeterStyle = {
             meter: {
-                margin: 8,
+                marginLeft: 30,
             },
             color: {
-                foreground: '#4184cc',
+                foreground: '#59b655',
+                background: '#1b1c1d',
+                bright: '#ffffff',
+            }
+        };
+
+        const overallMeterStyle = {
+            meter: {
+                marginLeft: 15,
+            },
+            color: {
+                foreground: '#59b655',
                 background: '#1b1c1d',
                 bright: '#ffffff',
             }
@@ -41,40 +53,163 @@ class Landing extends React.Component {
 
         return (
             <Container fluid className='padding'>
-                <Grid className='black-background'>
+                <Grid className='map-background'>
                     <Grid.Row>
                         <Grid.Column width={3}>
                             <Grid.Row className='padding'>
                                 <h1 className='heco'>HECO[EV]</h1>
                             </Grid.Row>
-                            <Grid.Row className='padding' centered>
-                                <CircleMeter value={89}
-                                             size={175}
-                                             foregroundColor={meterStyle.color.foreground}
-                                             backgroundColor={meterStyle.color.background}
-                                             style={meterStyle.meter}/>
-                            </Grid.Row>
-                            <Grid.Row className='padding'>
-                                <Menu pointing vertical inverted>
-                                    <Menu.Item
-                                        name='Dashboard'
-                                    />
-                                    <Menu.Item
-                                        name='User Audit'
-                                    />
-                                    <Menu.Item
-                                        name='Settings'
-                                    />
-                                    <Menu.Item
-                                        name='Logout'
-                                    />
-                                </Menu>
-                            </Grid.Row>
+                            <Segment inverted>
+                                <Grid.Row className='padding' centered>
+                                    <h4>Overall Station Health</h4>
+                                    <CircleMeter value={88}
+                                                 size={150}
+                                                 foregroundColor={overallMeterStyle.color.foreground}
+                                                 backgroundColor={overallMeterStyle.color.background}
+                                                 style={overallMeterStyle.meter}/>
+                                </Grid.Row>
+                            </Segment>
+                            <Segment inverted>
+                                <Grid.Row className='padding'>
+                                    <List divided inverted relaxed>
+                                        <List.Item>
+                                            <List.Icon name='map marker' color={'green'}/>
+                                            <List.Content>
+                                                <List.Header as='a'>Haleiwa Town Center Charging Station</List.Header>
+                                                <List.Description>
+                                                    Haleiwa
+                                                </List.Description>
+                                            </List.Content>
+                                        </List.Item>
+                                        <List.Item>
+                                            <List.Icon name='map marker' color={'green'}/>
+                                            <List.Content>
+                                                <List.Header as='a'>Dole Plantation Charging Station</List.Header>
+                                                <List.Description>
+                                                    Wahiawa
+                                                </List.Description>
+                                            </List.Content>
+                                        </List.Item>
+                                        <List.Item>
+                                            <List.Icon name='map marker' color={'green'}/>
+                                            <List.Content>
+                                                <List.Header as='a'>Waianae Shopping Mall Charging Station</List.Header>
+                                                <List.Description>
+                                                    Waianae
+                                                </List.Description>
+                                            </List.Content>
+                                        </List.Item>
+                                        <List.Item>
+                                            <List.Icon name='map marker' color={'red'}/>
+                                            <List.Content>
+                                                <List.Header as='a'>Kapolei Commons Charging Station</List.Header>
+                                                <List.Description>
+                                                    Kapolei
+                                                </List.Description>
+                                            </List.Content>
+                                        </List.Item>
+                                        <List.Item>
+                                            <List.Icon name='map marker' color={'green'}/>
+                                            <List.Content>
+                                                <List.Header as='a'>Times Square Shopping Center Charging
+                                                    Station</List.Header>
+                                                <List.Description>
+                                                    Waipahu
+                                                </List.Description>
+                                            </List.Content>
+                                        </List.Item>
+                                        <List.Item>
+                                            <List.Icon name='map marker' color={'green'}/>
+                                            <List.Content>
+                                                <List.Header as='a'>Koolau Center Charging Station</List.Header>
+                                                <List.Description>
+                                                    Kaneohe
+                                                </List.Description>
+                                            </List.Content>
+                                        </List.Item>
+                                        <List.Item>
+                                            <List.Icon name='map marker' color={'green'}/>
+                                            <List.Content>
+                                                <List.Header as='a'>Iwilei Costco Parking Lot Charging
+                                                    Station</List.Header>
+                                                <List.Description>
+                                                    Honolulu
+                                                </List.Description>
+                                            </List.Content>
+                                        </List.Item>
+                                        <List.Item>
+                                            <List.Icon name='map marker' color={'green'}/>
+                                            <List.Content>
+                                                <List.Header as='a'>Hawaiian Electric Ward Office Charging
+                                                    Station</List.Header>
+                                                <List.Description>
+                                                    Honolulu
+                                                </List.Description>
+                                            </List.Content>
+                                        </List.Item>
+                                        <List.Item>
+                                            <List.Icon name='map marker' color={'green'}/>
+                                            <List.Content>
+                                                <List.Header as='a'>Hawaii Kai 7-Eleven Charging Station</List.Header>
+                                                <List.Description>
+                                                    Hawaii Kai
+                                                </List.Description>
+                                            </List.Content>
+                                        </List.Item>
+                                    </List>
+                                </Grid.Row>
+                            </Segment>
                         </Grid.Column>
 
 
                         <Grid.Column width={10}>
                             <Grid divided='vertically'>
+                                <Grid.Row columns={4} centered>
+                                    <Grid.Column>
+                                        <Segment inverted>
+                                            <h4>Active Sessions</h4>
+                                            <DiskMeter
+                                                value={25}
+                                                size={100}
+                                                backgroundColor={useMeterStyle.color.foreground}
+                                                textColor={useMeterStyle.color.foreground}
+                                                borderColor={useMeterStyle.color.foreground}
+                                                style={useMeterStyle.meter}
+                                            />
+                                        </Segment>
+                                    </Grid.Column>
+                                    <Grid.Column>
+                                        <Segment inverted>
+                                            <h4>Current Total Watts</h4>
+                                            <ReactSpeedometer
+                                                width={170}
+                                                height={100}
+                                                maxValue={500}
+                                                value={473}
+                                                needleColor="red"
+                                                startColor="green"
+                                                segments={10}
+                                                endColor="blue"
+                                            />
+                                        </Segment>
+                                    </Grid.Column>
+                                    <Grid.Column>
+                                        <Segment inverted>
+                                            <h4>Total Revenue</h4>
+                                            <h2>$26,179</h2>
+                                            <Grid.Row>
+                                                <p className='pBold'>24% <Icon name='arrow circle down' size='small' color='red'/></p>
+                                                <p>(from previous period)</p>
+                                            </Grid.Row>
+                                        </Segment>
+                                    </Grid.Column>
+                                    <Grid.Column>
+                                        <Segment inverted>
+                                            <h4>Payment Types</h4>
+                                        </Segment>
+                                    </Grid.Column>
+                                </Grid.Row>
+
                                 <Grid.Row columns={1}>
                                     <Grid.Column>
                                         <div className="embed-container">
@@ -93,10 +228,14 @@ class Landing extends React.Component {
 
                                 <Grid.Row columns={2}>
                                     <Grid.Column>
-                                        <Bar data={barData}/>
+                                        <Segment inverted>
+                                            <Bar data={barData}/>
+                                        </Segment>
                                     </Grid.Column>
                                     <Grid.Column>
-                                        <Line data={lineData}/>
+                                        <Segment inverted>
+                                            <Line data={lineData}/>
+                                        </Segment>
                                     </Grid.Column>
                                 </Grid.Row>
                             </Grid>
@@ -104,42 +243,41 @@ class Landing extends React.Component {
 
 
                         <Grid.Column width={3}>
-                            <Grid.Row>
-                                <ReactSpeedometer
-                                    width={200}
-                                    height={200}
-                                    value={333}
-                                    segments={5}
-                                    segmentColors={[
-                                        "#bf616a",
-                                        "#d08770",
-                                        "#ebcb8b",
-                                        "#a3be8c",
-                                        "#b48ead",
-                                    ]}
-                                />
-                            </Grid.Row>
-                            <Grid.Row>
-                                <ReactSpeedometer
-                                    width={200}
-                                    height={200}
-                                    maxValue={500}
-                                    value={473}
-                                    needleColor="red"
-                                    startColor="green"
-                                    segments={10}
-                                    endColor="blue"
-                                />
-                            </Grid.Row>
-                            <Grid.Row>
-                                <ReactSpeedometer
-                                    width={200}
-                                    height={200}
-                                    customSegmentStops={[0, 500, 750, 900, 1000]}
-                                    segmentColors={["firebrick", "tomato", "gold", "limegreen"]}
-                                    value={333}
-                                />
-                            </Grid.Row>
+                            <Segment inverted>
+                                <Grid.Row className='padding' centered>
+                                    <h4>Overall Station Health</h4>
+                                    <CircleMeter value={88}
+                                                 size={150}
+                                                 foregroundColor={overallMeterStyle.color.foreground}
+                                                 backgroundColor={overallMeterStyle.color.background}
+                                                 style={overallMeterStyle.meter}/>
+                                </Grid.Row>
+                            </Segment>
+                            <Segment inverted>
+                                <Grid.Row>
+                                    <ReactSpeedometer
+                                        width={200}
+                                        height={125}
+                                        maxValue={500}
+                                        value={473}
+                                        needleColor="red"
+                                        startColor="green"
+                                        segments={10}
+                                        endColor="blue"
+                                    />
+                                </Grid.Row>
+                            </Segment>
+                            <Segment inverted>
+                                <Grid.Row>
+                                    <ReactSpeedometer
+                                        width={200}
+                                        height={125}
+                                        customSegmentStops={[0, 500, 750, 900, 1000]}
+                                        segmentColors={["firebrick", "tomato", "gold", "limegreen"]}
+                                        value={333}
+                                    />
+                                </Grid.Row>
+                            </Segment>
                         </Grid.Column>
                     </Grid.Row>
                 </Grid>
