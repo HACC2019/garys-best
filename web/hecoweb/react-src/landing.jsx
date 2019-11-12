70902378538e07835ee911ffad23d7e150bccc3a
Mercedez Castro <mercedez@hawaii.edu>
2019-11-12 13:53:25 -1000

    Layout update

--- web/hecoweb/react-src/landing.jsx
+++ web/hecoweb/react-src/landing.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import 'semantic-ui-css/semantic.css';
 import ReactSpeedometer from "react-d3-speedometer";
-import { CircleMeter, DiskMeter } from 'react-svg-meters'
+import { CircleMeter, DiskMeter, BlockMeter } from 'react-svg-meters'
 import { Bar, Line } from 'react-chartjs-2';
-import { Grid, Container, List, Segment, Message, Icon } from 'semantic-ui-react'
+import { Grid, Container, List, Segment, Icon } from 'semantic-ui-react'
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
@@ -31,7 +31,7 @@ class Landing extends React.Component {
 
         const useMeterStyle = {
             meter: {
-                marginLeft: 30,
+                marginLeft: 40,
             },
             color: {
                 foreground: '#59b655',
@@ -40,9 +40,20 @@ class Landing extends React.Component {
             }
         };
 
+        const avgMeterStyle = {
+            meter: {
+                marginLeft: 40,
+            },
+            color: {
+                foreground: '#59b655',
+                background: '#3d4044',
+                bright: '#1b1c1d',
+            }
+        };
+
         const overallMeterStyle = {
             meter: {
-                marginLeft: 15,
+                marginLeft: 30,
             },
             color: {
                 foreground: '#59b655',
@@ -59,6 +70,16 @@ class Landing extends React.Component {
                             <Grid.Row className='padding'>
                                 <h1 className='heco'>HECO[EV]</h1>
                             </Grid.Row>
+                            <Segment inverted>
+                                <Grid.Row className='padding' centered>
+                                    <h4>Overall Station Health</h4>
+                                    <CircleMeter value={88}
+                                                 size={125}
+                                                 foregroundColor={overallMeterStyle.color.foreground}
+                                                 backgroundColor={overallMeterStyle.color.background}
+                                                 style={overallMeterStyle.meter}/>
+                                </Grid.Row>
+                            </Segment>
                             <Segment inverted>
                                 <Grid.Row className='padding'>
                                     <List divided inverted relaxed>
@@ -153,25 +174,11 @@ class Landing extends React.Component {
 
 
                         <Grid.Column width={10}>
-                            <Grid divided='vertically'>
-                                    <Grid.Row columns={2}>
-                                        <Grid.Column>
-                                            <Segment inverted>
-                                                <Bar data={barData}/>
-                                            </Segment>
-                                        </Grid.Column>
-                                        <Grid.Column>
-                                            <Segment inverted>
-                                                <Line data={lineData}/>
-                                            </Segment>
-                                        </Grid.Column>
-                                    </Grid.Row>
-
+                            <Grid>
                                 <Grid.Row columns={1}>
                                     <Grid.Column>
-                                        <div className="embed-container">
                                             <iframe width="850"
-                                                    height="600"
+                                                    height="425"
                                                     frameBorder="0"
                                                     scrolling="no"
                                                     marginHeight="0"
@@ -179,29 +186,44 @@ class Landing extends React.Component {
                                                     title="EV Map"
                                                     src="//mercedezcastro.maps.arcgis.com/apps/Embed/index.html?webmap=f4c533c1a6d04d98a2b2d37277c7c160&extent=-158.361,21.2603,-157.5521,21.6208&zoom=true&previewImage=false&scale=true&disable_scroll=true&theme=light">
                                             </iframe>
-                                        </div>
                                     </Grid.Column>
                                 </Grid.Row>
+
+                                <Grid.Row columns={2}>
+                                    <Grid.Column>
+                                        <Segment inverted>
+                                            <Bar data={barData}/>
+                                        </Segment>
+                                    </Grid.Column>
+                                    <Grid.Column>
+                                        <Segment inverted>
+                                            <Line data={lineData}/>
+                                        </Segment>
+                                    </Grid.Column>
+                                </Grid.Row>
+                                <Grid.Row columns={2}>
+                                    <Grid.Column>
+                                        <Segment inverted>
+                                            <Line data={lineData}/>
+                                        </Segment>
+                                    </Grid.Column>
+                                    <Grid.Column>
+                                        <Segment inverted>
+                                            <Bar data={barData}/>
+                                        </Segment>
+                                    </Grid.Column>
+                                </Grid.Row>
+
                             </Grid>
                         </Grid.Column>
-                        
+
                         <Grid.Column width={3}>
-                            <Segment inverted>
-                                <Grid.Row className='padding' centered>
-                                    <h4>Overall Station Health</h4>
-                                    <CircleMeter value={88}
-                                                 size={150}
-                                                 foregroundColor={overallMeterStyle.color.foreground}
-                                                 backgroundColor={overallMeterStyle.color.background}
-                                                 style={overallMeterStyle.meter}/>
-                                </Grid.Row>
-                            </Segment>
                             <Segment inverted>
                                 <Grid.Row>
                                     <h4>Active Sessions</h4>
                                     <DiskMeter
                                         value={25}
-                                        size={100}
+                                        size={125}
                                         backgroundColor={useMeterStyle.color.foreground}
                                         textColor={useMeterStyle.color.foreground}
                                         borderColor={useMeterStyle.color.foreground}
@@ -213,14 +235,27 @@ class Landing extends React.Component {
                                 <Grid.Row>
                                     <h4>Current Total Watts</h4>
                                     <ReactSpeedometer
-                                        width={170}
-                                        height={100}
+                                        width={200}
+                                        height={125}
                                         maxValue={500}
                                         value={473}
-                                        needleColor="red"
+                                        needleColor="black"
                                         startColor="green"
-                                        segments={10}
+                                        segments={4}
                                         endColor="blue"
+                                    />
+                                </Grid.Row>
+                            </Segment>
+                            <Segment inverted>
+                                <h4>Average Charge Duration</h4>
+                                <Grid.Row>
+                                    <BlockMeter
+                                        value={75}
+                                        size={125}
+                                        foregroundColor={avgMeterStyle.color.foreground}
+                                        backgroundColor={avgMeterStyle.color.background}
+                                        textColor={avgMeterStyle.color.bright}
+                                        style={avgMeterStyle.meter}
                                     />
                                 </Grid.Row>
                             </Segment>
