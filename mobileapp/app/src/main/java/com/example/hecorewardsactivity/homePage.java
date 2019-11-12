41a609eaec06af33a3ce68bbc4414adf326ed5b2
Irene Ma <mairene@hawaii.edu>
2019-11-12 02:17:19 -1000

    Homepage redesign

--- mobileapp/app/src/main/java/com/example/hecorewardsactivity/homePage.java
+++ mobileapp/app/src/main/java/com/example/hecorewardsactivity/homePage.java
@@ -98,11 +98,7 @@ public class homePage extends AppCompatActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+        
 
         return super.onOptionsItemSelected(item);
     }
